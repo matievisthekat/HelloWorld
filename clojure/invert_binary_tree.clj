d6cb2d93250d656b7e3913e31e062a176eeb2c7c
matievisthekat <matievisthekat@gmail.com>
2022-12-01 11:50:53 +0200

    add binary tree example

---
+++ clojure/invert_binary_tree.clj
@@ -0,0 +1,30 @@
+(require '[clojure.pprint])
+
+(defn invert-binary-tree [node]
+  (if (nil? node)
+    nil
+    (let [node-left (get node "left"), node-right (get node "right")]
+      (merge node {"left" (invert-binary-tree node-right), "right" (invert-binary-tree node-left)}))))
+
+(def tree {
+           "id" 1
+           "left" {
+                   "id" 3
+                   "left" {
+                           "id" 5
+                           }
+                   "right" {
+                            "id" 2
+                            }}
+           "right" {
+                    "id" 4
+                    "left" {
+                            "id" 6
+                            }
+                    "right" {
+                             "id" 8
+                             }
+                    }
+           })
+(def inverted (invert-binary-tree tree))
+(clojure.pprint/pprint inverted)
