f40c33320718ac4c30520f501dba33cd8e7f241a
matievisthekat <matievisthekat@gmail.com>
2022-12-02 11:10:28 +0200

    add comments

--- clojure/invert_binary_tree.clj
+++ clojure/invert_binary_tree.clj
@@ -1,10 +1,19 @@
 (require '[clojure.pprint])
 
 (defn invert-binary-tree [node]
-  (if (nil? node)
+  (if (nil? node) ;; Check if the node is nil. If it is, return nil
     nil
+    ;; Get the "left" and "right" values from the node
     (let [node-left (get node :left), node-right (get node :right)]
-      (merge node {:left (invert-binary-tree node-right), :right (invert-binary-tree node-left)}))))
+      ;; Here we merge the previous node with our new node we just created
+      ;; merging allows us to keep the other properties (other than "left" and "right") the same on our new node
+      (merge
+        node
+        {
+          :left (invert-binary-tree node-right),
+          :right (invert-binary-tree node-left)
+         }
+       ))))
 
 (def tree {
           :id 1
